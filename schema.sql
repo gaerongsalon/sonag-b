688340fe58f17c44344888115579d10852f576a9
jaeyoung.choi <lactrious@gmail.com>
2024-07-18 00:43:31 +0900

    Update schema

--- schema.sql
+++ schema.sql
@@ -2,15 +2,17 @@
 -- CREATE USER 'sonagb'@'%' IDENTIFIED BY 'PASSWORD';
 -- GRANT ALL PRIVILEGES ON sonagb.* TO 'sonagb'@'%' WITH GRANT OPTION;
 CREATE TABLE
-  `sonagb`.`hello` (
+  `sonagb`.`account` (
     `seq` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(80) NOT NULL UNIQUE,
+    `email` VARCHAR(160) NOT NULL UNIQUE,
+    `name` VARCHAR(80) NOT NULL,
     PRIMARY KEY (`seq`)
   );
   
 CREATE TABLE
   `sonagb`.`stage` (
     `seq` INT NOT NULL AUTO_INCREMENT,
+    `accountSeq` INT NOT NULL,
     `name` VARCHAR(80) NOT NULL UNIQUE,
     `data` TEXT NOT NULL,
     PRIMARY KEY (`seq`)
@@ -20,10 +22,10 @@ CREATE TABLE
   `sonagb`.`scoreboard` (
     `seq` INT NOT NULL AUTO_INCREMENT,
     `stageSeq` INT NOT NULL,
-    `userName` VARCHAR(80) NOT NULL,
+    `accountSeq` INT NOT NULL,
     `score` INT NOT NULL,
     PRIMARY KEY (`seq`)
   );
 
 ALTER TABLE `sonagb`.`scoreboard`
-	ADD INDEX `scoreboard_stageScore` (`stageSeq` ASC, `score` DESC);
+	ADD INDEX `scoreboard_stageScore` (`score` DESC, `stageSeq` DESC);
